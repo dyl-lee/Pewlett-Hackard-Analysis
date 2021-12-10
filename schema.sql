980ad56f61b8452656ee3a58040379663251ec98
dyl-lee <dylanlee1701@gmail.com>
2021-12-10 01:18:14 -0500

    comments

--- schema.sql
+++ schema.sql
@@ -54,6 +54,47 @@ CREATE TABLE Dept_Employees (
 	PRIMARY KEY (dept_no, emp_no)
 );
 
--- query the database
-select *
-FROM departments;
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Create new table from retirement eligibility
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- drop retirement_info table, this table was a query so no connections to sever 
+DROP TABLE retirement_info;
+
+-- Create new table for retiring employees, now includes emp_no for joining
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- inner join departments and dept_manager
+SELECT departments.dept_name, 
+	dept_manager.emp_no, 
+	dept_manager.from_date, 
+	dept_manager.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm 
+ON d.dept_no = dm.dept_no;
+
+
+-- make join between retirement_info and dept_emp, attempt 1.
+-- Left join in this way returned with 331603 rows and a bunch of nulls
+-- substituting right join returned with 45784 rows and no nulls
+SELECT dept_employees.to_date,
+	retirement_info.emp_no,
+	retirement_info.first_name,
+	retirement_info.last_name
+FROM dept_employees as d
+LEFT JOIN retirement_info as ri ON ri.emp_no = d.emp_no
