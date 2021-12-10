252aa631aab9bc36bfe9070cf5ff18a829851c64
dyl-lee <dylanlee1701@gmail.com>
2021-12-10 01:06:41 -0500

    7.3.2 completed

--- schema.sql
+++ schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE departments(
 CREATE TABLE Employees (
 	emp_no INT NOT NULL,
 	birth_date DATE NOT NULL,
-	first_name VARCHAR(10) NOT NULL,
+	first_name VARCHAR(20) NOT NULL,
 	last_name VARCHAR(20) NOT NULL,
 	gender VARCHAR(10) NOT NULL,
 	hire_date DATE NOT NULL,
@@ -17,13 +17,13 @@ CREATE TABLE Employees (
 );
 
 CREATE TABLE dept_manager (
-	emp_no INT NOT NULL,
 	dept_no VARCHAR(4) NOT NULL,
+	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no, dept_no)
+		PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE salaries (
@@ -35,13 +35,13 @@ CREATE TABLE salaries (
 	PRIMARY KEY (emp_no)
 );
 
+-- has no row with unique values, yet mod 7.1.5 ERD suggests emp_no, title and from_date are all keys. Are they foreign keys?
 create table titles (
 	emp_no INT NOT NULL,
 	title VARCHAR(20) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE Dept_Employees (
